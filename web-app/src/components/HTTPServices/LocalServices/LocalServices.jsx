19b16e4548d2635fcfcc2f136eafba6bb4c7840d
Irvyn Cornejo <irvyncornejo@gmail.com>
2022-12-11 16:54:05 -0600

    Replace type of fetch

--- web-app/src/components/HTTPServices/LocalServices/LocalServices.jsx
+++ web-app/src/components/HTTPServices/LocalServices/LocalServices.jsx
@@ -1,6 +1,6 @@
 class LocalServices{
   changeState = async (path, queryParams) => {
-    await fetch(`https://192.168.1.141/`, { mode: 'no-cors', method: 'POST' })
+    await fetch(`http://${path}${queryParams}`, { mode: 'no-cors', method: 'POST' })
   }
 }
 
